618eb2a49f8afb31f145302fe71173bc32bd4981
Abdul <abdul.tanveer@gmail.com>
2023-12-19 16:24:31 +0800

    scaffold screen

--- app/src/main/java/com/example/composedemo/jetpackcompose/screens/layouts/ScaffoldScreen.kt
+++ app/src/main/java/com/example/composedemo/jetpackcompose/screens/layouts/ScaffoldScreen.kt
@@ -34,7 +34,34 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.composedemo.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -48,9 +75,19 @@ fun ScaffoldScreen() {
   }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyScaffold() {
-  //TODO write your code here
+  ScaffoldExample()
+  /*val snackbarHostState = remember { SnackbarHostState() }
+  val scope = rememberCoroutineScope()
+
+  Scaffold(contentColor = colorResource(id = R.color.purple_700),
+    topBar = { MyTopAppBar()},
+    bottomBar = { MyBottomAppBar()},
+    content = ()
+  )*/
+
 }
 
 @Composable
@@ -62,4 +99,61 @@ fun MyTopAppBar() {
 fun MyBottomAppBar() {
   //TODO write your code here
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+  var presses by remember { mutableStateOf(0) }
+
+  Scaffold(
+    topBar = {
+      TopAppBar(
+    /*    colors = TopAppBarColors(
+          containerColor = MaterialTheme.colorScheme.primaryContainer,
+          titleContentColor = MaterialTheme.colorScheme.primary,
+        ),*/
+        title = {
+          Text("Top app bar")
+        }
+      )
+    },
+    bottomBar = {
+      BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.primary,
+      ) {
+        Text(
+          modifier = Modifier
+            .fillMaxWidth(),
+          textAlign = TextAlign.Center,
+          text = "Bottom app bar",
+        )
+      }
+    },
+    floatingActionButton = {
+      FloatingActionButton(onClick = { presses++ }) {
+        Icon(Icons.Default.Add, contentDescription = "Add")
+      }
+    }
+  ) { innerPadding ->
+    Column(
+      modifier = Modifier
+        .padding(innerPadding),
+      verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+      Text(
+        modifier = Modifier.padding(8.dp),
+        text =
+        """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button $presses times.
+                """.trimIndent(),
+      )
+    }
+  }
+}
+
 

643b4073f64116aad8ae5d3976b4ab9180573748
Abdul <abdul.tanveer@gmail.com>
2023-12-19 16:31:34 +0800

    scaffold screen -- topappbar colors fixed

--- app/src/main/java/com/example/composedemo/jetpackcompose/screens/layouts/ScaffoldScreen.kt
+++ app/src/main/java/com/example/composedemo/jetpackcompose/screens/layouts/ScaffoldScreen.kt
@@ -50,6 +50,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -112,6 +113,10 @@ fun ScaffoldExample() {
           containerColor = MaterialTheme.colorScheme.primaryContainer,
           titleContentColor = MaterialTheme.colorScheme.primary,
         ),*/
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+          containerColor = MaterialTheme.colorScheme.primaryContainer,
+          titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
         title = {
           Text("Top app bar")
         }
